571cfd74820705118f3987b2f3e30e6d065c6239
mytoandeptrai <mytoandn@gmail.com>
2021-02-06 00:04:57 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ShoppingCart from './views/ShoppingCart';
+
+function App() {
+  return (
+    <div className="App">
+      <ShoppingCart />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/ShoppingCart.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ShoppingCart = () => {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/" rel="noreferrer" target="_blank">
+          React Shopping Cart
+        </a>
+      </header>
+      <main>Product List</main>
+      <footer>All right is reserved</footer>
+    </div>
+  );
+};
+
+export default ShoppingCart;
